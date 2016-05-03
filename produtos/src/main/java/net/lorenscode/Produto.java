7cabec1101db9d5b4d03a9e83708d3f194079e08
Lourenco Lima da Cunha <llc.from.hell@gmail.com>
2016-05-03 12:43:55 -0300

    aula 01 e 02

---
+++ produtos/src/main/java/net/lorenscode/Produto.java
@@ -0,0 +1,17 @@
+package net.lorenscode;
+
+public class Produto {
+	
+	private final String nome;
+	private final double preco;
+	
+	public Produto(String nome, double preco) {
+		super();
+		this.nome = nome;
+		this.preco = preco;
+	}
+	
+	
+	
+
+}
